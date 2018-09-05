04434fbe65352317030647917cf21848b2390ae9
wellington <wellingtonpoloniosilva@gmail.com>
2018-09-04 22:43:54 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessExpeption.java
@@ -0,0 +1,12 @@
+package chess;
+
+public class ChessExpeption extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	public ChessExpeption(String msg) {
+		super(msg);
+		
+	}
+	
+
+}
